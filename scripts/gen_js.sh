c86d3fee5b6488704cd9c428d78424bafcb62b88
Thomas Hallgren <thomas@tada.se>
2018-08-13 10:28:47 +0200

    removed package-lock.json

--- scripts/gen_js.sh
+++ scripts/gen_js.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # generate js codes via grpc-tools
 
 PUPPET_PATH=${GOPATH}/src/github.com/puppetlabs
@@ -8,12 +9,12 @@ PROTO_PATH=fsmpb/fsm.proto
 --js_out=import_style=commonjs,binary:dist/src \
 --grpc_out=dist/src \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
--I $PUPPET_PATH/data-protobuf:$PUPPET_PATH/go-fsm \
+-I ${PUPPET_PATH}/data-protobuf:${PUPPET_PATH}/go-fsm \
 datapb/data.proto fsmpb/fsm.proto
 
 # generate d.ts codes
 protoc \
 --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
 --ts_out=src \
--I $PUPPET_PATH/data-protobuf:$PUPPET_PATH/go-fsm \
+-I ${PUPPET_PATH}/data-protobuf:${PUPPET_PATH}/go-fsm \
 datapb/data.proto fsmpb/fsm.proto
