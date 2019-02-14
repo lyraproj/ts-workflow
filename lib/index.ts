6c6878775c1ef1f3c6ac956fddd982ddd99b5456
Thomas Hallgren <thomas@tada.se>
2019-02-14 10:57:14 +0100

    Make package export servicebuilder module

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,1 @@
-import {Sensitive} from './pcore/Sensitive';
-import {Namespace, TypedName} from './pcore/TypedName';
-import {Service} from './servicesdk/Service';
-
-const service = new Service({Sensitive, TypedName}, new TypedName(Namespace.NsService, 'Lyra::Workflow'), 2000, 21000);
-service.start();
+export * from './servicesdk/ServiceBuilder';
